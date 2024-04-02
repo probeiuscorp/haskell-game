6bdff5892de3b348c05f4fd1ab66836a2d5ec86e
Caleb Stimpson <caleb.stimpson@gmail.com>
2024-04-02 08:04:37 -0500

    Open blank window

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,30 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
-import Lib
+import qualified SDL
+import SDL.Vect
+import Control.Monad (unless)
 
+-- Adapted from https://github.com/haskell-game/sdl2/blob/master/examples/twinklebear/Lesson04.hs
 main :: IO ()
-main = putStrLn "Hello world"
+main = do
+    SDL.initialize [ SDL.InitVideo ]
+    let winConfig = SDL.defaultWindow { SDL.windowInitialSize = V2 800 600 }
+    
+    window <- SDL.createWindow "Wheel of Time" winConfig
+    renderer <- SDL.createRenderer window (-1) SDL.defaultRenderer
+
+    let loop = do
+          SDL.present renderer
+          quit <- fmap (\ev -> case SDL.eventPayload ev of
+              SDL.QuitEvent -> True
+              SDL.KeyboardEvent e -> SDL.keyboardEventKeyMotion e ==  SDL.Pressed
+              SDL.MouseButtonEvent e -> SDL.mouseButtonEventMotion e == SDL.Pressed
+              _ -> False) SDL.waitEvent
+          unless quit loop
+    loop
+
+    SDL.destroyRenderer renderer
+    SDL.destroyWindow window
+    SDL.quit
