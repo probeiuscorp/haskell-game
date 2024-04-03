399a7920f45abd64f211b9d703b9fe12a780a5f6
Caleb Stimpson <caleb.stimpson@gmail.com>
2024-04-03 07:24:50 -0500

    Paint image on window

--- app/Main.hs
+++ app/Main.hs
@@ -6,6 +6,20 @@ import qualified SDL
 import SDL.Vect
 import Control.Monad (unless)
 
+getDataFileName :: FilePath -> IO FilePath
+getDataFileName = return
+
+loadTexture :: SDL.Renderer -> FilePath -> IO SDL.Texture
+loadTexture renderer path = do
+    bmp <- SDL.loadBMP path
+    SDL.createTextureFromSurface renderer bmp <* SDL.freeSurface bmp
+
+renderTexture :: SDL.Renderer -> SDL.Texture -> IO ()
+renderTexture renderer texture = do
+    ti <- SDL.queryTexture texture
+    let (w, h) = (SDL.textureWidth ti, SDL.textureHeight ti)
+    SDL.copy renderer texture Nothing (Just $ SDL.Rectangle (P $ V2 300 300) (V2 w h))
+
 -- Adapted from https://github.com/haskell-game/sdl2/blob/master/examples/twinklebear/Lesson04.hs
 main :: IO ()
 main = do
@@ -14,9 +28,12 @@ main = do
     
     window <- SDL.createWindow "Wheel of Time" winConfig
     renderer <- SDL.createRenderer window (-1) SDL.defaultRenderer
+    image <- getDataFileName "blast.bmp" >>= loadTexture renderer
 
     let loop = do
+          renderTexture renderer image
           SDL.present renderer
+          
           quit <- fmap (\ev -> case SDL.eventPayload ev of
               SDL.QuitEvent -> True
               SDL.KeyboardEvent e -> SDL.keyboardEventKeyMotion e ==  SDL.Pressed
